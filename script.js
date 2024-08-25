51e54a7c0fe005e97533aae5cded09ea15c98a2e
Raj Kalyan Kumar <rajkalyan.dev@gmail.com>
2024-08-25 11:49:28 +0530

    arrays

---
+++ script.js
@@ -0,0 +1,20 @@
+// Arrays
+
+// Arrays in programmin language are the collection of elements in one place, where the accessing of each element is based on Index value which starts from 0 to n-1.
+
+// Sizes of DataTypes in JS and C
+
+// JS
+// Numbers -> 8bytes
+// Strings -> Each character takes 2bytes
+// Objects -> Depends on number of properties and size of each property
+// Arrays -> Depends on number of elements and size of each element
+// Boolean -> Even though it requires just 1Bit but takes up 1byte
+
+// C
+// Int -> 4bytes
+// Float -> 4bytes
+// char -> 1byte
+// Long -> 8bytes
+// Double -> 8bytes
+
