0e063a794ff7e956fbfce0131a1fac2ddad264de
Raj Kalyan Kumar <rajkalyan.dev@gmail.com>
2024-09-06 00:42:21 +0530

    optimized insertion sort

--- Sorting/InsertionsSort.js
+++ Sorting/InsertionsSort.js
@@ -9,18 +9,12 @@ let arr = [5,4,3,2,1,0];
 
 for(let i = 0; i < arr.length - 1; i++){
     let temp = arr[i + 1];
-    let flag = 0;
     let j = i;
-    for(;j >= 0; j--){
-        if(arr[j] > temp){
-            arr[j + 1] = arr[j];
-            flag = 1;
-        }
-        else break;
+    while(j >= 0 && arr[j] > temp){
+        arr[j + 1] = arr[j];
+        j--;
     }
-    if(flag === 1){
-        arr[j + 1] = temp;
-    }
+    arr[j + 1] = temp;
 }
 
 console.log(...arr)
